ccd896ad1bfc7d4977552b8b1979d1b5b6c242e3
Ian Ker-Seymer <i.kerseymer@gmail.com>
2021-05-05 12:52:10 -0400

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,6 +6,12 @@ version="$1"
 git_tag="v$version"
 docker_tag="ianks/prometheus-exporter-customerio:$git_tag"
 
+fastmod "version: 0.1.2" "version: $version" ./charts
+fastmod "appVersion: 0.1.2" "version: $version" ./charts
+
+git commit -am "Bump to $git_tag :confetti_ball:"
+git push origin master
+
 docker build . --tag "$docker_tag"
 docker push "$docker_tag"
 
